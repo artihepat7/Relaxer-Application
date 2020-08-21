fb0a47d49d45ca26ec502741e838b6a1f6f3ccc4
artihepat7 <hepatarti@gmail.com>
2020-08-21 18:06:49 +0530

    Relaxer Application

---
+++ script.js
@@ -0,0 +1,22 @@
+const text = document.getElementById("text");
+const container = document.getElementById("container");
+
+const totalTime = 7500;
+const breathe = 3000;
+const hold = 1500;
+InitRelaxerApp();
+function InitRelaxerApp() {
+  text.innerText = "Breathe In";
+  container.className = " container grow";
+  setTimeout(() => {
+    text.innerText = "hold";
+    setTimeout(() => {
+      container.className = " container shrink";
+      text.innerText = "breathe out";
+    }, hold);
+  }, breathe);
+}
+
+setInterval(() => {
+  InitRelaxerApp();
+}, totalTime);
